dda341aaf1b8419cc5fb8aee7ed08a81aecd65a8
Emily Krohn <emilykrohn2@gmail.com>
2025-02-03 20:39:42 -0800

    Idle in left, right, up, down direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveDirection = Vector2.zero;
     private InputAction move;
     private Rigidbody2D rb;
+    private string previousDirection = "Down";
 
     void Awake()
     {
@@ -39,6 +40,10 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
             animator.SetBool("Idle", false);
+            animator.SetBool("IdleRight", false);
+            animator.SetBool("IdleLeft", false);
+            animator.SetBool("IdleUp", false);
+            previousDirection = "Right";
         }
         else if (moveDirection.x < 0)
         {
@@ -47,6 +52,10 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
             animator.SetBool("Idle", false);
+            animator.SetBool("IdleRight", false);
+            animator.SetBool("IdleLeft", false);
+            animator.SetBool("IdleUp", false);
+            previousDirection = "Left";
         }
         else if (moveDirection.y > 0)
         {
@@ -55,6 +64,10 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Up", true);
             animator.SetBool("Down", false);
             animator.SetBool("Idle", false);
+            animator.SetBool("IdleRight", false);
+            animator.SetBool("IdleLeft", false);
+            animator.SetBool("IdleUp", false);
+            previousDirection = "Up";
         }
         else if (moveDirection.y < 0)
         {
@@ -63,14 +76,38 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Up", false);
             animator.SetBool("Down", true);
             animator.SetBool("Idle", false);
+            animator.SetBool("IdleRight", false);
+            animator.SetBool("IdleLeft", false);
+            animator.SetBool("IdleUp", false);
+            previousDirection = "Down";
         }
-        else if (moveDirection.x == 0)
+        else if (moveDirection.x == 0 && moveDirection.y == 0)
         {
             animator.SetBool("Right", false);
             animator.SetBool("Left", false);
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
-            animator.SetBool("Idle", true);
+
+            animator.SetBool("Idle", false);
+            animator.SetBool("IdleRight", false);
+            animator.SetBool("IdleLeft", false);
+            animator.SetBool("IdleUp", false);
+            if (previousDirection == "Down")
+            {
+                animator.SetBool("Idle", true);
+            }
+            else if (previousDirection == "Up")
+            {
+                animator.SetBool("IdleUp", true);
+            }
+            else if (previousDirection == "Left")
+            {
+                animator.SetBool("IdleLeft", true);
+            }
+            else if (previousDirection == "Right")
+            {
+                animator.SetBool("IdleRight", true);
+            }
         }
     }
 
