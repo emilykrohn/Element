71f4051d6029896dc14ebb434e32cbbd9ebf1dee
Emily Krohn <emilykrohn2@gmail.com>
2025-02-14 16:29:49 -0800

    Add Encounter Cooldown

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    float timer = 0f;
+    public float cooldownEncounter = 3f;
+    
     void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Encounter"))
         {
-            Debug.Log("Encounter");
+            timer += Time.deltaTime;
+            if (timer > cooldownEncounter)
+            {
+                Debug.Log("Encounter");
+                timer = 0f;
+            }
         }
     }
 }
