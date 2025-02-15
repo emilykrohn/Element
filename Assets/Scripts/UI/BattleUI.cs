bba18bb07dade6dfb8c5d26abc37a0af3c39b07f
Emily Krohn <emilykrohn2@gmail.com>
2025-02-15 13:41:30 -0800

    Attack Button Brings Up Attack Options
    The main buttons with that attack, item, and escape button are hidden and the attack buttons are shown.

--- Assets/Scripts/UI/BattleUI.cs
+++ Assets/Scripts/UI/BattleUI.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,8 @@ using UnityEngine.UIElements;
 public class BattleUI : MonoBehaviour
 {
     UnityEngine.UIElements.Button attackButton;
+    UnityEngine.UIElements.VisualElement mainButtonsContainer;
+    UnityEngine.UIElements.VisualElement attackButtonsContainer;
 
     void OnEnable()
     {
@@ -13,6 +16,9 @@ public class BattleUI : MonoBehaviour
 
         attackButton = uiDoc.rootVisualElement.Q("AttackButton") as UnityEngine.UIElements.Button;
         attackButton.RegisterCallback<ClickEvent>(OpenAttackMenu);
+
+        mainButtonsContainer = uiDoc.rootVisualElement.Q("MainButtons") as UnityEngine.UIElements.VisualElement;
+        attackButtonsContainer = uiDoc.rootVisualElement.Q("AttackButtons") as UnityEngine.UIElements.VisualElement;
     }
 
     void OnDisable()
@@ -22,6 +28,7 @@ public class BattleUI : MonoBehaviour
 
     void OpenAttackMenu(ClickEvent evt)
     {
-        Debug.Log("AttackMenu");
+        mainButtonsContainer.style.display = DisplayStyle.None;
+        attackButtonsContainer.style.display = DisplayStyle.Flex;
     }
 }
