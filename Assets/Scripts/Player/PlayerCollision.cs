988bf76f21d63ed7616a8ca79afa814236cf8c48
Emily Krohn <emilykrohn2@gmail.com>
2025-03-11 08:57:46 -0700

    Create House
    The name of the house will tell the House Scene which house object it should display and hide the other house objects.

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -5,6 +5,7 @@ public class PlayerCollision : MonoBehaviour
 {
     float timer = 0f;
     public float cooldownEncounter = 3f;
+    public string current_house = "";
     
     void OnTriggerStay2D(Collider2D collision)
     {
@@ -15,6 +16,14 @@ public class PlayerCollision : MonoBehaviour
             {
                 timer = 0f;
                 SceneManager.LoadScene("BattleScene");
+            }
+        }
+        else if (collision.CompareTag("House"))
+        {
+            if (collision.name == "Scene01House01")
+            {
+                current_house = "Scene01House01";
+                SceneManager.LoadScene("HouseScene");
             }
         }
     }
